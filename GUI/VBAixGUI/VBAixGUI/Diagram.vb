7d22ce19c3fa602ba3582e2272a7067d6cf87bab
Janik Horst <janik.horst@rwth-aachen.de>
2021-07-06 18:15:46 +0200

    Added Diagram Element

---
+++ GUI/VBAixGUI/VBAixGUI/Diagram.vb
@@ -0,0 +1,171 @@
+﻿Option Strict Off
+
+Imports System.ComponentModel
+Public Class Diagram
+
+#Region "Events"
+
+#End Region
+
+#Region "Properties"
+
+    <Browsable(True), Description("Poll Rate")> Dim _pollRate As Integer = 1000
+    <Browsable(True), Description("X-Axis min")> Dim _xAxis_min As Decimal = 0
+    <Browsable(True), Description("X-Axis max")> Dim _xAxis_max As Decimal = 100
+    <Browsable(True), Description("X-Axis logaritmic")> Dim _xAxis_log As Boolean = 0
+    <Browsable(True), Description("Y-Axis min")> Dim _yAxis_min As Decimal = 0
+    <Browsable(True), Description("Y-Axis max")> Dim _yAxis_max As Decimal = 100
+    <Browsable(True), Description("Y-Axis logaritmic")> Dim _yAxis_log As Boolean = 0
+    <Browsable(True), Description("X-Axis Name")> Dim _xAxis_Name As String = "X-Axis"
+    <Browsable(True), Description("Y-Axis Name")> Dim _yAxis_Name As String = "Y-Axis"
+
+    Private _symbolList As New List(Of SeriesSelection)
+#End Region
+
+#Region "Property Functions"
+    Public Property PollRate() As Integer
+        Get
+            PollRate = _pollRate
+        End Get
+        Set(ByVal Value As Integer)
+            If _pollRate <> Value Then
+                If Value <= 0 Then
+                    TimerPoll.Stop()
+                    _pollRate = 0
+                ElseIf Value > 0 Then
+                    TimerPoll.Start()
+                    TimerPoll.Interval = Value
+                    _pollRate = Value
+                Else
+                End If
+                Me.Invalidate()
+            End If
+        End Set
+    End Property
+
+    Public Property XAxis_Name() As String
+        Get
+            XAxis_Name = _xAxis_Name
+        End Get
+        Set(ByVal value As String)
+            If _xAxis_Name <> value Then
+                _xAxis_Name = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+    Public Property XAxis_min() As Decimal
+        Get
+            XAxis_min = _xAxis_min
+        End Get
+        Set(ByVal value As Decimal)
+            If _xAxis_min <> value Then
+                _xAxis_min = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+
+    Public Property XAxis_max() As Decimal
+        Get
+            XAxis_max = _xAxis_max
+        End Get
+        Set(ByVal value As Decimal)
+            If _xAxis_max <> value Then
+                _xAxis_max = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+    Public Property XAxis_log() As Boolean
+        Get
+            XAxis_log = _xAxis_log
+        End Get
+        Set(ByVal value As Boolean)
+            If _xAxis_log <> value Then
+                _xAxis_log = value
+                If _xAxis_log = True And _xAxis_min = 0 Then
+                    _xAxis_min = 0.01
+                End If
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+    Public Property YAxis_min() As Decimal
+        Get
+            YAxis_min = _yAxis_min
+        End Get
+        Set(ByVal value As Decimal)
+            If _yAxis_min <> value Then
+                _yAxis_min = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+
+    Public Property YAxis_max() As Decimal
+        Get
+            YAxis_max = _yAxis_max
+        End Get
+        Set(ByVal value As Decimal)
+            If _yAxis_max <> value Then
+                _yAxis_max = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+    Public Property YAxis_log() As Boolean
+        Get
+            YAxis_log = _yAxis_log
+        End Get
+        Set(ByVal value As Boolean)
+            If _yAxis_log <> value Then
+                _yAxis_log = value
+                If _yAxis_log = True And _yAxis_min = 0 Then
+                    _yAxis_min = 0.01
+                End If
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+    Public Property YAxis_Name() As String
+        Get
+            YAxis_Name = _yAxis_Name
+        End Get
+        Set(ByVal value As String)
+            If _yAxis_Name <> value Then
+                _yAxis_Name = value
+                AxisAdjust()
+            End If
+        End Set
+    End Property
+
+#End Region
+
+#Region "Subs"
+    Private Sub AxisAdjust()
+        If _xAxis_log = True Then
+            Chart1.ChartAreas(0).AxisX.IsLogarithmic = True
+        Else Chart1.ChartAreas(0).AxisX.IsLogarithmic = False
+        End If
+        If _yAxis_log = True Then
+            Chart1.ChartAreas(0).AxisY.IsLogarithmic = True
+        Else Chart1.ChartAreas(0).AxisY.IsLogarithmic = False
+        End If
+        Chart1.ChartAreas(0).AxisX.Minimum = _xAxis_min
+        Chart1.ChartAreas(0).AxisX.Maximum = _xAxis_max
+        Chart1.ChartAreas(0).AxisY.Minimum = _yAxis_min
+        Chart1.ChartAreas(0).AxisY.Maximum = _yAxis_max
+        Chart1.ChartAreas(0).AxisX.Title = _xAxis_Name
+        Chart1.ChartAreas(0).AxisY.Title = _yAxis_Name
+    End Sub
+
+
+#End Region
+
+
+#Region "Form Events"
+
+#End Region
+
+End Class
